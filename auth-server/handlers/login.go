a9c1164779246bd3240f0848f7bd04d3d50b7cd1
Rob Dominguez <rob.dominguez@hasura.io>
2024-06-10 21:04:49 -0500

    Update connection URI

--- auth-server/handlers/login.go
+++ auth-server/handlers/login.go
@@ -1,11 +1,11 @@
 package handlers
 
 import (
-	// "fmt"
-	// "log"
+	"fmt"
+	"log"
 	"net/http"
 
-	// "dominguezdev.com/auth-server/graphqlClient"
+	"dominguezdev.com/auth-server/graphqlClient"
 	"github.com/gin-gonic/gin"
 )
 
@@ -30,32 +30,32 @@ func LoginHandler(c *gin.Context) {
 	// If it does, we'll check the password; if it doesn't we'll create a new user.
 	// If the password is correct, we'll send them a token as a response; if they're new,
 	// they'll get one, too.
-	// client := graphqlClient.CreateClient()
-	//
-	// // Here's our GraphQL query to check if a user exists
-	// query := `
-	// 	query($username: string!) {
-	//      user_users {
-	//        id
-	//        username
-	//      }
-	// 	}
-	// `
-	//
-	// // Define variables for the query
-	// variables := map[string]interface{}{
-	// 	"username": credentials.Username,
-	// }
-	//
-	// // Execute the query
-	// respData, err := graphqlClient.QueryHasura(client, query, variables)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	//
-	// // Print the response
-	// user := respData["user"].(map[string]interface{})
-	// fmt.Printf("User: %s\n", user["username"])
+	client := graphqlClient.CreateClient()
+
+	// Here's our GraphQL query to check if a user exists
+	query := `
+		query($username: string!) {
+	     user_users {
+	       id
+	       username
+	     }
+		}
+	`
+
+	// Define variables for the query
+	variables := map[string]interface{}{
+		"username": credentials.Username,
+	}
+
+	// Execute the query
+	respData, err := graphqlClient.QueryHasura(client, query, variables)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the response
+	user := respData["user"].(map[string]interface{})
+	fmt.Printf("User: %s\n", user["username"])
 
 	// If everything is good, let's give them a token
 	tokenString, err := GenerateJWT(credentials.Username)
