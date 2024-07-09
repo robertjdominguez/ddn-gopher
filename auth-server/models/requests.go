cea2ebd818a6e04188743680ea56f64fb5d40fa7
Rob Dominguez <rob.dominguez@hasura.io>
2024-07-09 15:00:14 -0500

    Refactor models into single files
    Moves the structs into their own files.

---
+++ auth-server/models/requests.go
@@ -0,0 +1,5 @@
+package models
+
+type Request struct {
+	Token string `json:"token"`
+}
---
+++ auth-server/models/tokens.go
@@ -0,0 +1,7 @@
+package models
+
+type DecodedToken struct {
+	Username string
+	UserId   float64
+	IsValid  bool
+}
