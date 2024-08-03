eb2981b30efdb7c5124d34c20a89bfa81aa6c723
Rob Dominguez <rob.dominguez@hasura.io>
2024-08-03 08:45:34 -0500

    Add name to users table
    Adds name to users table so that, when displaying the profile, there's at least something unique to the user.

--- hasura/user/connector/base_pg/db/seed.sql
+++ hasura/user/connector/base_pg/db/seed.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
-INSERT INTO users (username, password) VALUES ('exampleuser', 'examplepassword');
-INSERT INTO users (username, password) VALUES ('exampleuser1', 'examplepassword1');
+INSERT INTO users (username, password, name) VALUES ('exampleuser', 'examplepassword', 'Hasura');
+INSERT INTO users (username, password, name) VALUES ('exampleuser1', 'examplepassword1', 'Gopher');
